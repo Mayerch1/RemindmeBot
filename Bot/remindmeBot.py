a011a6e7ece25f1e320a6b434d46a386bbc24cbe
Christian <it@cj-mayer.de>
2021-07-17 00:56:01 +0200

    improve timezone selection and help pages

--- Bot/remindmeBot.py
+++ Bot/remindmeBot.py
@@ -7,11 +7,16 @@ from discord_slash.utils.manage_commands import create_option, create_choice
 from discord_slash.model import SlashCommandOptionType
 
 import difflib
+from datetime import datetime
 from dateutil import tz
 from dateutil.zoneinfo import getzoneinfofile_stream, ZoneInfoFile
 
+from pytz import common_timezones as pytz_common_timezones
+
 from lib.Connector import Connector
 from lib.Analytics import Analytics
+
+from ReminderModule import ReminderModule
 
 
 intents = discord.Intents()
@@ -53,28 +58,108 @@ async def get_timezone(ctx):
 
 
 async def set_timezone(ctx, value):
-    tz_obj = tz.gettz(value)
 
-    if tz_obj:
-        Connector.set_timezone(ctx.guild.id, value)
+    def get_tz_error_str(zone, closest_tz):
 
-        out_str = 'Timezone is now set to `{:s}`'.format(value)
-        if re.match(r'^UTC\+\d+$', value):
-            out_str += '\n*Consider using your local timezone (instead of `UTC+X`), in order to automatically adjust to daylight-saving*'
-
-        await ctx.send(out_str)
-    else:
-
-        err = True
-        err_str = 'The timezone `{:s}` is not valid'.format(value)
-
-        all_zones = list(ZoneInfoFile(getzoneinfofile_stream()).zones.keys())
-        closest_tz = difflib.get_close_matches(value, all_zones)
-
+        err_str = 'The timezone `{:s}` is not valid'.format(zone)
         if closest_tz:
             err_str += '\nDid you mean `{:s}`?'.format('`, `'.join(closest_tz))
 
-        await ctx.send(err_str, hidden=True)
+        err_str += '\n\nYou can have a look at all available timezones on this wikipedia list '\
+                  'https://en.wikipedia.org/wiki/List_of_tz_database_time_zones'
+
+        return err_str
+
+    def get_tz_error_eb(zone, closest_tz):
+
+        if closest_tz:
+            tz_propose = 'Did you mean `{:s}`?'.format('`, `'.join(closest_tz))
+        else:
+            tz_propose = ''
+
+        eb = discord.Embed(title='Invalid Timezone configuration',
+                           color=0xde4b55,
+                           description=f'The timezone `{zone}` is not valid\n'\
+                                       f'{tz_propose}\n'\
+                                       f'You can have a look at all available timezones on this wikipedia '\
+                                        '[list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)')
+
+        return eb
+
+
+    def get_tz_info_str(zone, name):
+
+        offset = datetime.now(zone).strftime('%z')
+        out_str = f'Timezone is now set to `{name}` (`UTC{offset}`)'
+
+        if re.match(r'^UTC\+\d+$', name):
+            out_str += '\n• Consider using your local timezone (instead of `UTC+X`), in order to automatically adjust to daylight-saving*'
+        elif name.lower() == 'mst':
+            out_str += '\n• Consider using `MST7MDT` to respect daylight saving during winter'
+        elif name.lower() == 'est':
+            out_str += '\n• Consider using `EST5EDT` to respect daylight saving during winter'
+
+        if name not in pytz_common_timezones:
+            out_str += f'\n• `{name}` seems to be a deprecated timezone and could be discontinued in future versions.\n'\
+                       f'• Try and use a geo-referenced timezone that _observes_ `{name}` instead (e.g. `Europe/Berlin`)'
+
+        return out_str
+
+
+    def get_tz_info_eb(zone, name):
+
+        offset = datetime.now(zone).strftime('%z')
+
+        info_str = ''
+        if re.match(r'^UTC\+\d+$', name):
+            info_str += '\n• Consider using your local timezone (instead of `UTC+X`), in order to automatically adjust to daylight-saving*'
+        elif name.lower() == 'mst':
+            info_str += '\n• Consider using `MST7MDT` to respect daylight saving during winter'
+        elif name.lower() == 'est':
+            info_str += '\n• Consider using `EST5EDT` to respect daylight saving during winter'
+
+        if name not in pytz_common_timezones:
+            info_str += f'\n• `{name}` seems to be a deprecated timezone and could be discontinued in future versions.\n'\
+                       f'• Try and use a geo-referenced timezone that _observes_ `{name}` instead (e.g. `Europe/Berlin`)'
+
+        if not info_str:
+            # no warnings means green embed
+            col = 0x69eb67
+        else:
+            # some non-critical warnings will show slightly yellow embed
+            col = 0xcceb67
+
+        eb = discord.Embed(title='Timezone configuration',
+                           color=col,
+                           description=f'The timezone is now set to `{name}` (`UTC{offset}`)\n'\
+                                       f'{info_str}')
+        
+        return eb
+
+
+    tz_obj = tz.gettz(value)
+  
+    if tz_obj:
+        Connector.set_timezone(ctx.guild.id, value)
+
+        try:
+            await ctx.send(embed=get_tz_info_eb(tz_obj, value))
+        except discord.errors.Forbidden as e:
+            await ctx.send(get_tz_info_str(tz_obj, value))
+
+    else:
+        all_zones = list(ZoneInfoFile(getzoneinfofile_stream()).zones.keys())
+        closest_tz = difflib.get_close_matches(value, all_zones, n=4)
+
+        if value.lower() == 'pst':
+            closest_tz = ['PST8PDT']  # manual override
+        elif value.lower() == 'cst':
+            closest_tz = ['CST6CDT']  # manual override
+
+        try:
+            await ctx.send(embed=get_tz_error_eb(value, closest_tz), hidden=True)
+        except discord.errors.Forbidden as e:
+            await ctx.send(get_tz_error_str(value, closest_tz), hidden=True)
         
 
 
@@ -119,30 +204,112 @@ async def set_timezone_cmd(ctx, mode, timezone=None):
 
         await set_timezone(ctx, timezone)
 
-@client.slash.slash(name='help', description='Show the help page for this bot')
-async def get_help(ctx):
 
-    embed = discord.Embed(title='Remindme Help', description='Reminding you whenever you want')
+@client.slash.slash(name='help', description='Show the help page for this bot',
+                    options=[
+                        create_option(
+                            name='page',
+                            description='choose the subpage to display',
+                            required=False,
+                            option_type=SlashCommandOptionType.STRING,
+                            choices=[
+                                create_choice(
+                                    name='overview',
+                                    value='overview'
+                                ),
+                                create_choice(
+                                    name='syntax',
+                                    value='syntax'
+                                ),
+                                create_choice(
+                                    name='timezones',
+                                    value='timezones'
+                                )
+                            ]
+                        )
+                    ])
+async def get_help(ctx, page='overview'):
 
-    embed.add_field(name='/help', value='show this message', inline=False)
-    embed.add_field(name='/timezone', value='set/get the timezone of this server', inline=False)
-    embed.add_field(name='/remindme', value='reminding you after a set time period', inline=False)
-    embed.add_field(name='/remind', value='remind another user after a set time period', inline=False)
-    embed.add_field(name='/reminder_list', value='manage all reminders of this server', inline=False)
+    def get_overview_eb():
 
-    embed.add_field(name='\u200b', value='If you like this bot, you can leave a vote at [top.gg](https://top.gg/bot/831142367397412874)', inline=False)
+        embed = discord.Embed(title='Remindme Help', description='Reminding you whenever you want')
+
+        embed.add_field(name='/help [page]', value='show the help page or choose a submenu [page]', inline=False)
+        embed.add_field(name='/timezone', value='set/get the timezone of this server', inline=False)
+        embed.add_field(name='/remindme', value='reminding you after a set time period', inline=False)
+        embed.add_field(name='/remind', value='remind another user after a set time period', inline=False)
+        embed.add_field(name='/reminder_list', value='manage all reminders of this server', inline=False)
+
+        embed.add_field(name='\u200b', value='If you like this bot, you can leave a vote at [top.gg](https://top.gg/bot/831142367397412874)', inline=False)
+
+        return embed
+
+    def get_overview_str():
+        return '**Remindme Help**\n'\
+                '```Reminding you whenever you want\n'\
+                '\n'\
+                'help          Shows this message\n'\
+                'timezone      set/get the timezone of this server\n'\
+                'remindme      reminding you after a set time period\n'\
+                'remind        remind another user after a set time period\n'\
+                'reminder_list manage all your reminders for this server\n\n'\
+                'please assign \'Embed Links\' permissions for better formatting```'
+
+    def get_timezone_eb():
+
+        eb = discord.Embed(title='Remindme Help', description='timezone help')
+
+        eb.add_field(name='/timezone get', value='get the current timezone', inline=False)
+        eb.add_field(name='/timezone set', value='set a new timezone', inline=False)
+
+        eb.add_field(name='\u200B', 
+                    value='• Allowed timezones are any strings defined by the IANA\n'\
+                          '• Some timezones are marked as \'deprecated\' but can be used with a warning\n'\
+                          '• geo-referencing timezones (e.g. `Europe/Berlin`) should be preferred\n'\
+                          '  over more general (and deprecated) timezones (e.g. `CET`)', inline=False)
+
+        eb.add_field(name='\u200b', value='If you like this bot, you can leave a vote at [top.gg](https://top.gg/bot/831142367397412874)', inline=False)
+
+        return eb
+
+    def get_timezone_str():
+        return '**Remindme Help** - timezones\n'\
+               '```'\
+               '/timezone get     get the current timezone\n'\
+               '/timezone set     set a new timezone\n\n'\
+               '• Allowed timezones are any strings defined by the IANA\n'\
+               '• Some timezones are marked as \'deprecated\' but can be used with a warning\n'\
+               '• geo-referencing timezones (Europe/Berlin) should be preferred\n'\
+               '  over more general (and deprecated) timezones (CET)'\
+               '```'
+
+    def get_syntax_eb():
+
+        eb = discord.Embed(title='Remindme Help', 
+                           description='This syntax is used whenever a user invokes `/remindme` or `/remind`\n'\
+                                      f'{ReminderModule.REMIND_FORMAT_HELP}\n'\
+                                      f'{ReminderModule.HELP_FOOTER}')
+        
+        return eb
+
+    def get_syntax_str():
+        return '**Remindme Help** - parser syntax and example usage' + ReminderModule.REMIND_FORMAT_HELP
+
+
+    if page == 'overview':
+        eb = get_overview_eb()
+        fallback = get_overview_str()
+    elif page == 'syntax':
+        eb = get_syntax_eb()
+        fallback = get_syntax_str()
+    elif page == 'timezones':
+        eb = get_timezone_eb()
+        fallback = get_timezone_str()
 
     try:
-        await ctx.send(embed=embed)
+        await ctx.send(embed=eb)
     except discord.errors.Forbidden:
-        await ctx.send('```Reminding you whenever you want\n'\
-                    '\n'\
-                    'help          Shows this message\n'\
-                    'timezone      set/get the timezone of this server\n'\
-                    'remindme      reminding you after a set time period\n'\
-                    'remind        remind another user after a set time period\n'\
-                    'reminder_list manage all your reminders for this server\n\n'
-                    'please assign \'Embed Links\' permissions for better formatting```')
+        await ctx.send(fallback)
 
 
 @client.event
